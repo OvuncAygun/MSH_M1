666f4cd7e61d39ecd929bbf5627d3455960c5d80
Övünç Aygün <ovunc.aygun@gmail.com>
2025-12-11 20:23:41 +0300

    Added empty implementation for initialization interface to test CMake

---
+++ src/MSH_M1.cpp
@@ -0,0 +1,12 @@
+#include "MSH_M1.hpp"
+
+MSH_M1::MSH_M1() = default;
+MSH_M1::~MSH_M1() = default;
+
+std::vector<IDeviceFactory*> MSH_M1::getDeviceFactories() {
+    return {};
+}
+
+std::vector<IProductFamilyFactory*> MSH_M1::getProductFamilyFactories() {
+    return {};
+}

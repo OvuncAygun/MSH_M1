d9d44afe76e87a06bf54ce10874e1153571cfc1e
Övünç Aygün <ovunc.aygun@gmail.com>
2025-12-16 18:22:05 +0300

    Added CMake configurations and main header functions

--- src/MSH_M1.cpp
+++ src/MSH_M1.cpp
@@ -1,12 +1,27 @@
 #include "MSH_M1.h"
 
+#include "AlarmFactory.h"
+#include "CameraFactory.h"
+#include "LightFactory.h"
+#include "MusicFactory.h"
+#include "TVFactory.h"
+#include "DetectorFactory.h"
+
 MSH_M1::MSH_M1() = default;
 MSH_M1::~MSH_M1() = default;
 
 std::vector<IDeviceFactory*> MSH_M1::getDeviceFactories() {
-    return {};
+    std::vector<IDeviceFactory*> factories;
+    factories.push_back(new AlarmFactory());
+    factories.push_back(new CameraFactory());
+    factories.push_back(new LightFactory());
+    factories.push_back(new MusicFactory());
+    factories.push_back(new TVFactory());
+    return factories;
 }
 
 std::vector<IProductFamilyFactory*> MSH_M1::getProductFamilyFactories() {
-    return {};
+    std::vector<IProductFamilyFactory*> factories;
+    factories.push_back(new DetectorFactory());
+    return factories;
 }
